23b9561d260b814e37dd29ebb684e18b09bbc00c
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-06-11 20:07:24 -0600

    Added auth context

---
+++ client/CookBook/src/components/contexts/AuthContext.tsx
@@ -0,0 +1,77 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import firebase from "firebase/app";
+import {auth}  from '../../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({children}) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const value = {
+    currentUser
+  }
+
+  auth.onAuthStateChanged((user) => {
+    setCurrentUser(user);
+    setLoading(false);
+  });
+
+  return (
+    <AuthContext.Provider value={children}/>
+  )
+
+}
+// const firebaseConfig = {
+//   // Your Firebase config here
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// interface AuthContextProps {
+//   currentUser: firebase.User | null;
+//   signInWithGoogle: () => Promise<void>;
+//   signOut: () => Promise<void>;
+// }
+
+// const AuthContext = createContext<AuthContextProps>({
+//   currentUser: null,
+//   signInWithGoogle: async () => {},
+//   signOut: async () => {},
+// });
+
+// export function useAuth() {
+//   return useContext(AuthContext);
+// }
+
+// export function AuthProvider({ children }: { children: React.ReactNode }) {
+//   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
+
+//   useEffect(() => {
+//     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//     });
+
+//     return unsubscribe;
+//   }, []);
+
+//   async function signInWithGoogle() {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     await firebase.auth().signInWithPopup(provider);
+//   }
+
+//   async function signOut() {
+//     await firebase.auth().signOut();
+//   }
+
+//   const value = {
+//     currentUser,
+//     signInWithGoogle,
+//     signOut,
+//   };
+
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+// }
