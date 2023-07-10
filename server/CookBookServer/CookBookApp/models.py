e336c2699914254817cf909d7122d3761568aeb6
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-07-10 01:09:25 -0600

    COnfigured user database models

--- server/CookBookServer/CookBookApp/models.py
+++ server/CookBookServer/CookBookApp/models.py
@@ -5,16 +5,12 @@ class UserProfile(models.Model):
     firebase_uid = models.CharField(max_length=255, primary_key=True)
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    # Add additional user profile fields here
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey('UserProfile', related_name='posts', on_delete=models.CASCADE)
-    likes = models.ManyToManyField('UserProfile', related_name='liked_posts')
-    shared = models.ManyToManyField('UserProfile', related_name='shared_posts')
-    saved = models.ManyToManyField('UserProfile', related_name='saved_posts')
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -23,5 +19,20 @@ class Comment(models.Model):
     post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey('UserProfile', related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Save(models.Model):
+    user = models.ForeignKey('UserProfile', related_name='saved_posts', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', related_name='saved_posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey('UserProfile', related_name='followings', on_delete=models.CASCADE)
+    followed = models.ForeignKey('UserProfile', related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
