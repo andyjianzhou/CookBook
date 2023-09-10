dfb79a8a24d1b447c161344fdf825bfc38998bac
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-09-10 01:35:12 -0700

    Update IPostService

--- client/CookBook/src/components/Services/IPostServices.tsx
+++ client/CookBook/src/components/Services/IPostServices.tsx
@@ -1,3 +1,16 @@
 interface IPostServices {
-    
+    createPost: (post: any) => Promise<any>;
+    editPost: (id: string, post: any) => Promise<any>;
+    getPosts: () => Promise<any>;
+    getPost: (id: string) => Promise<any>;
+    updatePost: (id: string, post: any) => Promise<any>;
+    deletePost: (id: string) => Promise<any>;
+    createComment: (id: string, comment: any) => Promise<any>;
+    deleteComment: (id: string, commentId: string) => Promise<any>;
+    likePost: (id: string) => Promise<any>;
+    unlikePost: (id: string) => Promise<any>;
+    likeComment: (id: string, commentId: string) => Promise<any>;
+    unlikeComment: (id: string, commentId: string) => Promise<any>;
+    replyComment: (id: string, commentId: string, reply: any) => Promise<any>;
+    deleteReply: (id: string, commentId: string, replyId: string) => Promise<any>;
 }
