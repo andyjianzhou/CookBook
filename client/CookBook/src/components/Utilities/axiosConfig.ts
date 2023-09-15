9019e544b10474b9c67a2886c025778c90ddf808
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-09-14 20:13:03 -0700

    Removing csrf token

--- client/CookBook/src/components/Utilities/axiosConfig.ts
+++ client/CookBook/src/components/Utilities/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const csrfToken = getCookie('csrftoken');
-console.log(csrfToken)
+
 const axiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api',
     withCredentials: true, // This ensures cookies are sent with requests
