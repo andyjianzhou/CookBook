b37ef7abe7606d3b81f0f1f937a7f19047610a2e
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-11-14 14:51:40 -0800

    Adding pintura

--- client/CookBook/src/components/Scan/CapturedImage.tsx
+++ client/CookBook/src/components/Scan/CapturedImage.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import { PinturaEditor } from '@pqina/react-pintura';
+import '@pqina/pintura/pintura.css';
+import { getEditorDefaults } from '@pqina/pintura';
 
 type CapturedImageProps = {
   image: string;
 }
 
 const CapturedImage: React.FC<CapturedImageProps> = ({ image }) => {
+  const [inlineResult, setInlineResult] = useState();
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img src={image} alt="captured" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+      <PinturaEditor>
+        {...getEditorDefaults()}
+        <img src={image} alt="captured" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+      </PinturaEditor>
     </div>
   );
 }
