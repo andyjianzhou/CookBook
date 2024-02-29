3607a7cc5cbec3b2c2e85c470261b1bc0aeaff05
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2024-02-28 20:26:40 -0800

    Updated settings.py

--- server/CookBookServer/CookBookServer/settings.py
+++ server/CookBookServer/CookBookServer/settings.py
@@ -146,12 +146,14 @@ SESSION_COOKIE_SECURE = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",  # frontend server
-    "https://cook-book-hpdu.vercel.app"
+    "https://cook-book-hpdu.vercel.app",
+    "http://localhost:5173",  # frontend server
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
-    "https://cook-book-hpdu.vercel.app"
+    "https://cook-book-hpdu.vercel.app",
+    "http://localhost:5173",
 ]
 
 # media_files blob
